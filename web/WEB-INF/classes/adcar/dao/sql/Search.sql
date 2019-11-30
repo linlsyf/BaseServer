e9664f8da71fcf38744743b44fe2536ebe63d59a
linlsyf <linlsyf@gmail.com>
2019-11-30 10:52:45 +0800

    回滚

--- web/WEB-INF/classes/adcar/dao/sql/Search.sql
+++ web/WEB-INF/classes/adcar/dao/sql/Search.sql
@@ -1,14 +1,7 @@
 SELECT * FROM adcar
         WHERE 1=1
 
-<<<<<<< HEAD
-SELECT * FROM adcar        WHERE 1=1                    order  by  createtime desc
-
-
- <#if start??&& limit??>
-=======
       <#if start??&& limit??>
->>>>>>> parent of e4665e7... 添加登录限制
        limit ${start},${limit}
         </#if>
 
