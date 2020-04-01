2bc620b2f99f96efecc46ea1c03d0126a4a9ba67
linlsyf <linlsyf@gmail.com>
2020-04-01 18:04:40 +0800

    博客更新

--- modules/easy/src/main/java/blog/dao/sql/Update.sql
+++ modules/easy/src/main/java/blog/dao/sql/Update.sql
@@ -1,21 +1,9 @@
 
 UPDATE blog SET
 
-<#if title??> title=${$(title)}, </#if>
-<#if content??> content=${$(content)}, </#if>
-<#if name??> NAME=${$(name)}, </#if>
-<#if basicSalary??> BASIC_SALARY=${$(basicSalary)}, </#if>
-<#if performSalary??> PERFORM_SALARY=${$(performSalary)}, </#if>
-<#if nextRank??> NEXT_RANK=${$(nextRank)}, </#if>
-<#if promotionPercent??> PROMOTION_PERCENT=${$(promotionPercent)}, </#if>
-<#if status??> STATUS=${$(status)}, </#if>
-<#if createTime??> CREATE_TIME=${$(createTime)}, </#if>
-<#if creatorId??> CREATOR_ID=${$(creatorId)}, </#if>
-<#if creator??> CREATOR=${$(creator)}, </#if>
-<#if modifierId??> MODIFIER_ID=${$(modifierId)}, </#if>
-<#if modifiedTime??> MODIFIED_TIME=${$(modifiedTime)}, </#if>
-<#if modifier??> MODIFIER=${$(modifier)}, </#if>
-<#if id??> ID=${$(id)} </#if>
- WHERE id = ${$(id)}
+<#if title??> title=${title}, </#if>
+<#if content??> content=${content} </#if>
+
+ WHERE id = ${id}
 
 
