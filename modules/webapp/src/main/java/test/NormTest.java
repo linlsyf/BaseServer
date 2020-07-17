553f7e08fc25f373c3e304c7fb7382e218b0d7a4
linlsyf <linlsyf@gmail.com>
2020-07-17 14:50:33 +0800

    99

--- modules/webapp/src/main/java/test/NormTest.java
+++ modules/webapp/src/main/java/test/NormTest.java
@@ -8,13 +8,35 @@ public class NormTest {
 
     public static  final  void main(String[] arg){
 
-        String word="c++";
-        String test="";
-        if (word.contains("+")){
-            test=word.replace("c++","C%2B%2B");
-        }
+//        String word="c++";
+//        String test="";
+//        if (word.contains("+")){
+//            test=word.replace("c++","C%2B%2B");
+//        }
+//
+//        System.out.println(test);
 
-        System.out.println(test);
+          int initNum=9;
+          int  n=9;
+
+          for(int i=1;i<=initNum;i++){
+              System.out.println("   ");
+              for(int k=1;k<i;k++){
+
+                  for(int j=i;j<=initNum;j++){
+
+                 System.out.println(""+i+"*"+j);
+
+
+                  }
+
+
+              }
+
+
+
+          }
+
     }
     public static  final  void main2(String[] arg){
 
