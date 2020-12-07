023fc950676d8dbcca68aa93d2f4f92d739d4d00
linlsyf <linlsyf@gmail.com>
2020-12-07 11:47:25 +0800

    修复更新问题

--- modules/easy/src/main/java/blog/dao/sql/Update.sql
+++ modules/easy/src/main/java/blog/dao/sql/Update.sql
@@ -2,8 +2,10 @@
 UPDATE blog SET
 
 <#if title??> title=${title}, </#if>
-<#if content??> content=${content}, </#if>
-<#if status??> status=${status} </#if>
+<#if content??>content=${content}, </#if>
+<#if status??> status=${status}, </#if>
+<#if typename??> typename=${typename}, </#if>
+<#if typecode??> typecode=${typecode} </#if>
 
  WHERE id = ${id}
 
