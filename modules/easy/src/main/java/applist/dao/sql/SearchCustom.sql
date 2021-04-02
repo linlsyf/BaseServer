26739d4a9ab08d3bb586a63fb1a03aeec38f1772
linlsyf <linlsyf@gmail.com>
2021-04-02 10:03:26 +0800

    添加自定义APPlist

---
+++ modules/easy/src/main/java/applist/dao/sql/SearchCustom.sql
@@ -0,0 +1,15 @@
+SELECT * FROM app_custom_list
+        WHERE 1=1
+
+ <#if type??>
+       and type=${type}
+        </#if>
+ <#if parent_id??>
+       and parent_id=${parent_id}
+       
+<#else> 
+  and parent_id is null
+</#if> 
+
+        order  by sn  desc
+
